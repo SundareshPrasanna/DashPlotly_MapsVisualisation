574df89c676c5a9b2b5fcb4e1346066a692ae881
SundareshChandran <55475055+SundareshChandran@users.noreply.github.com>
2021-07-15 20:56:07 +0530

    Update to DataTable to make it exportable

--- app3.py
+++ app3.py
@@ -7,11 +7,13 @@ import pandas as pd
 import plotly.graph_objs as go
 import numpy as np
 import plotly.express as px
+import dash_table
 
 first_dataset = pd.read_excel("test1.xlsx", engine='openpyxl')
 first_dataset['type_name'] = 'regular'
 second_dataset = pd.read_excel("test2.xlsx", engine='openpyxl')
 second_dataset['type_name'] = 'super'
+combined_dataset = first_dataset.append(second_dataset, ignore_index=True)
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
@@ -23,14 +25,29 @@ app.layout = html.Div([
         value='both',
         labelStyle={'display': 'inline-block'},
     ),
-    html.Div(style={"margin-top": "10px", "margin-bottom": "15px"}),
+    html.Div(style={"marginTop": "10px", "marginBottom": "15px"}),
     html.Div(dcc.Graph(id='map_viz'), style={'width': '100%', 'display': 'flex',
-                                             'align-items': 'center', 'justify-content': 'center'}),
-    html.Div(dcc.Graph(id='table_viz'), style={
-             'width': '50%', 'display': 'inline-block'}),
-    html.Div(dcc.Graph(id='indicator_viz'), style={
-             'width': '50%', 'display': 'inline-block'})
-])
+                                             'alignItems': 'center', 'justifyContent': 'center'}),
+    html.Div(className='row', children=[html.Div(
+        children=[dash_table.DataTable(
+            id='table_viz',
+            columns=[{'name': i, 'id': i}
+                     for i in sorted(combined_dataset.columns)],
+            data=combined_dataset.to_dict('records'),
+            sort_mode='multi',
+            export_format="csv",
+            style_table={'overflowX': 'scroll',
+                         'maxHeight': '400px'},
+            style_header={'backgroundColor': 'rgb(188, 74, 60)'},
+            style_cell={'textAlign': 'center', 'backgroundColor': 'rgb(54, 69, 79)',
+                        'color': 'white',
+                        'height': 'auto', 'minWidth': '80px', 'width': '60px', 'maxWidth': '80px',
+                        },
+            sort_by=[])], style={
+            'width': '50%'}),
+        html.Div(
+        children=[dcc.Graph(id='indicator_viz')], style={
+            'width': '40%'})], style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly'})])
 
 
 @app.callback(Output("map_viz", "figure"),
@@ -58,7 +75,7 @@ def display_map(type_of_store):
     return fig
 
 
-@app.callback([Output("table_viz", "figure"), Output("indicator_viz", "figure")],
+@app.callback([Output("table_viz", "data"), Output("indicator_viz", "figure")],
               [Input("map_viz", "selectedData"),
                Input("type_of_store", "value")])
 def callback(selection1, type_of_store):
@@ -77,7 +94,7 @@ def callback(selection1, type_of_store):
                 mode="number",
                 value=combined_dataset.type_name.value_counts()[0],
                 title=combined_dataset.type_name.value_counts().index[0],
-                domain={'x': [0, 0], 'y': [0, 0]}))
+                domain={'x': [0, 0], 'y': [0, 0]}, ))
         else:
             fig2 = go.Figure()
 
@@ -91,13 +108,8 @@ def callback(selection1, type_of_store):
                 value=combined_dataset.type_name.value_counts()[1],
                 title=combined_dataset.type_name.value_counts().index[1],
                 domain={'x': [0, 0], 'y': [0.5, 1]}))
-        fig = go.Figure(data=[go.Table(
-            header=dict(values=list(combined_dataset.columns),
-                        fill_color=headerColor,
-                        align='left'),
-            cells=dict(values=[combined_dataset.name, combined_dataset.address, combined_dataset.lat, combined_dataset.lon, combined_dataset.type_name],
-                       line_color='darkslategray', font=dict(color='darkslategray', size=11),
-                       align='left', fill_color=[[rowOddColor, rowEvenColor, rowOddColor, rowEvenColor, rowOddColor]*5],))])
+        data = combined_dataset.to_dict('records')
+
     else:
         combined_dataset = first_dataset.append(
             second_dataset, ignore_index=True)
@@ -128,15 +140,9 @@ def callback(selection1, type_of_store):
                 value=combined_dataset.type_name.value_counts()[1],
                 title=combined_dataset.type_name.value_counts().index[1],
                 domain={'x': [0, 0], 'y': [0.5, 1]}))
-        fig = go.Figure(data=[go.Table(
-            header=dict(values=list(combined_dataset.columns),
-                        fill_color=headerColor,
-                        align='left'),
-            cells=dict(values=[combined_dataset.name, combined_dataset.address, combined_dataset.lat, combined_dataset.lon, combined_dataset.type_name, ],
-                       line_color='darkslategray', font=dict(color='darkslategray', size=11),
-                       align='left', fill_color=[[rowOddColor, rowEvenColor, rowOddColor, rowEvenColor, rowOddColor]*5],))])
+        data = combined_dataset.to_dict('records')
 
-    return fig, fig2
+    return data, fig2
 
 
 app.run_server(debug=True, port=8000, host='127.0.0.1')
