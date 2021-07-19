214034a061f541bcc6eec9c5530dba8428c8d38d
SundareshChandran <55475055+SundareshChandran@users.noreply.github.com>
2021-07-19 11:00:18 +0530

    Better error handling + new exception fixes

--- app3.py
+++ app3.py
@@ -27,6 +27,10 @@ app.layout = html.Div([
         value='both',
         labelStyle={'display': 'inline-block'},
     ),
+    html.Div(dcc.ConfirmDialog(
+        id='confirm',
+        message='Danger danger! Are you sure you want to continue?',
+    )),
     html.Div(style={"marginTop": "10px", "marginBottom": "15px"}),
     html.Div(className='row',
              children=[
@@ -69,27 +73,30 @@ app.layout = html.Div([
                Input('table_viz', "derived_viewport_data")])
 def display_map(type_of_store, filter_value):
     combined_dataset = first_dataset.append(second_dataset, ignore_index=True)
-    if (filter_value is not None and len(filter_value) > 0):
-        filtered_rows = [x['name'] for x in filter_value]
-        combined_dataset = combined_dataset[combined_dataset['name'].isin(
-            filtered_rows)]
-    if type_of_store != "both":
-        combined_dataset = combined_dataset[combined_dataset['type_name']
-                                            == type_of_store]
-    fig = px.scatter_mapbox(
-        combined_dataset,
-        lon='lon',
-        lat='lat',
-        hover_name='name',
-        color='type_name',
-        hover_data=['name', 'address'],
-        zoom=9, height=500)
-    fig.update_layout(mapbox_style="open-street-map",
-                      title='Plotly Visualization - Upwork',
-                      height=800,
-                      width=1400,
-                      margin={"r": 0, "t": 30, "l": 0, "b": 0}
-                      )
+    try:
+        if (filter_value is not None and len(filter_value) > 0):
+            filtered_rows = [x['name'] for x in filter_value]
+            combined_dataset = combined_dataset[combined_dataset['name'].isin(
+                filtered_rows)]
+        if type_of_store != "both":
+            combined_dataset = combined_dataset[combined_dataset['type_name']
+                                                == type_of_store]
+        fig = px.scatter_mapbox(
+            combined_dataset,
+            lon='lon',
+            lat='lat',
+            hover_name='name',
+            color='type_name',
+            hover_data=['name', 'address'],
+            zoom=9, height=500)
+        fig.update_layout(mapbox_style="open-street-map",
+                          title='Plotly Visualization - Upwork',
+                          height=800,
+                          width=1400,
+                          margin={"r": 0, "t": 30, "l": 0, "b": 0}
+                          )
+    except:
+        raise dash.exceptions.PreventUpdate
 
     return fig
 
@@ -100,59 +107,24 @@ def display_map(type_of_store, filter_value):
                Input('table_viz', "derived_viewport_data")])
 def callback(selection1, type_of_store, filter_value):
     combined_dataset = first_dataset.append(second_dataset, ignore_index=True)
-    if (filter_value is not None and len(filter_value) > 0):
-        filtered_rows = [x['name'] for x in filter_value]
-        combined_dataset = combined_dataset[combined_dataset['name'].isin(
-            filtered_rows)]
-    if(selection1 is None):
-        if type_of_store != "both":
-            combined_dataset = combined_dataset[combined_dataset['type_name']
-                                                == type_of_store]
-            fig2 = go.Figure()
-
-            fig2.add_trace(go.Indicator(
-                mode="number",
-                value=combined_dataset.type_name.value_counts()[0],
-                title=combined_dataset.type_name.value_counts().index[0],
-                domain={'x': [0, 0], 'y': [0, 0]}, align='center'))
-
-        else:
-            fig2 = go.Figure()
-            fig2.add_trace(go.Indicator(
-                mode="number",
-                value=combined_dataset.type_name.value_counts()[0],
-                title=combined_dataset.type_name.value_counts().index[0],
-                domain={'x': [0, 0], 'y': [0, 0.5]},
-                align='center'))
-            fig2.add_trace(go.Indicator(
-                mode="number",
-                value=combined_dataset.type_name.value_counts()[1],
-                title=combined_dataset.type_name.value_counts().index[1],
-                domain={'x': [0, 0], 'y': [0.5, 1]},
-                align='center'))
-        data = combined_dataset.to_dict('records')
-
-    else:
-        combined_dataset = first_dataset.append(
-            second_dataset, ignore_index=True)
-        if type_of_store != "both":
-            combined_dataset = combined_dataset[combined_dataset['type_name']
-                                                == type_of_store]
-            selected_points = [x['hovertext'] for x in selection1['points']]
+    try:
+        if (filter_value is not None and len(filter_value) > 0):
+            filtered_rows = [x['name'] for x in filter_value]
             combined_dataset = combined_dataset[combined_dataset['name'].isin(
-                selected_points)]
-            fig2 = go.Figure()
-            fig2.add_trace(go.Indicator(
-                mode="number",
-                value=combined_dataset.type_name.value_counts()[0],
-                title=combined_dataset.type_name.value_counts().index[0],
-                domain={'x': [0, 0], 'y': [0, 0]},
-                align='center'))
-        if type_of_store == "both":
-            selected_points = [x['hovertext'] for x in selection1['points']]
-            combined_dataset = combined_dataset[combined_dataset['name'].isin(
-                selected_points)]
-            if len(selected_points) == 1:
+                filtered_rows)]
+        if(selection1 is None):
+            if type_of_store != "both":
+                combined_dataset = combined_dataset[combined_dataset['type_name']
+                                                    == type_of_store]
+                fig2 = go.Figure()
+
+                fig2.add_trace(go.Indicator(
+                    mode="number",
+                    value=combined_dataset.type_name.value_counts()[0],
+                    title=combined_dataset.type_name.value_counts().index[0],
+                    domain={'x': [0, 0], 'y': [0, 0]}, align='center'))
+
+            else:
                 fig2 = go.Figure()
                 fig2.add_trace(go.Indicator(
                     mode="number",
@@ -160,22 +132,68 @@ def callback(selection1, type_of_store, filter_value):
                     title=combined_dataset.type_name.value_counts().index[0],
                     domain={'x': [0, 0], 'y': [0, 0.5]},
                     align='center'))
-            else:
+                if combined_dataset.shape[0] > 1:
+                    fig2.add_trace(go.Indicator(
+                        mode="number",
+                        value=combined_dataset.type_name.value_counts()[1],
+                        title=combined_dataset.type_name.value_counts(
+                        ).index[1],
+                        domain={'x': [0, 0], 'y': [0.5, 1]},
+                        align='center'))
+            data = combined_dataset.to_dict('records')
+
+        else:
+            combined_dataset = first_dataset.append(
+                second_dataset, ignore_index=True)
+            if type_of_store != "both":
+                combined_dataset = combined_dataset[combined_dataset['type_name']
+                                                    == type_of_store]
+                selected_points = [x['hovertext']
+                                   for x in selection1['points']]
+                combined_dataset = combined_dataset[combined_dataset['name'].isin(
+                    selected_points)]
                 fig2 = go.Figure()
                 fig2.add_trace(go.Indicator(
                     mode="number",
                     value=combined_dataset.type_name.value_counts()[0],
-                    title=combined_dataset.type_name.value_counts(
-                    ).index[0],
-                    domain={'x': [0, 0], 'y': [0, 0.5]},
+                    title=combined_dataset.type_name.value_counts().index[0],
+                    domain={'x': [0, 0], 'y': [0, 0]},
                     align='center'))
-                fig2.add_trace(go.Indicator(
-                    mode="number",
-                    value=combined_dataset.type_name.value_counts()[1],
-                    title=combined_dataset.type_name.value_counts().index[1],
-                    domain={'x': [0, 0], 'y': [0.5, 1]},
-                    align='center'))
-        data = combined_dataset.to_dict('records')
+            if type_of_store == "both":
+                selected_points = [x['hovertext']
+                                   for x in selection1['points']]
+                combined_dataset = combined_dataset[combined_dataset['name'].isin(
+                    selected_points)]
+                if len(selected_points) == 1:
+                    fig2 = go.Figure()
+                    fig2.add_trace(go.Indicator(
+                        mode="number",
+                        value=combined_dataset.type_name.value_counts()[0],
+                        title=combined_dataset.type_name.value_counts(
+                        ).index[0],
+                        domain={'x': [0, 0], 'y': [0, 0.5]},
+                        align='center'))
+                else:
+                    fig2 = go.Figure()
+                    fig2.add_trace(go.Indicator(
+                        mode="number",
+                        value=combined_dataset.type_name.value_counts()[0],
+                        title=combined_dataset.type_name.value_counts(
+                        ).index[0],
+                        domain={'x': [0, 0], 'y': [0, 0.5]},
+                        align='center'))
+                    if combined_dataset.shape[0] > 1:
+                        fig2.add_trace(go.Indicator(
+                            mode="number",
+                            value=combined_dataset.type_name.value_counts()[1],
+                            title=combined_dataset.type_name.value_counts(
+                            ).index[1],
+                            domain={'x': [0, 0], 'y': [0.5, 1]},
+                            align='center'))
+            data = combined_dataset.to_dict('records')
+
+    except Exception as e:
+        raise dash.exceptions.PreventUpdate
 
     return data, fig2
 
